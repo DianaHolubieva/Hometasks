ff60f037dbbf0b461ffc150200c90fc44b97374b
Di748 <golubevadiana356@gmail.com>
2022-05-12 15:40:09 +0200

    $

---
+++ ht12.05/assets/JS/index.js
@@ -0,0 +1,39 @@
+// // Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому!
+// //
+// //     - Напишіть код,  котрий :
+// // -- отримує текст з параграфа з id "content"
+//
+// let getContent = document.getElementById('content');
+// let content = getContent.innerText;
+//
+// // -- отримує текст з блоку з id "rules"
+//
+// let getRules = document.getElementById('rules');
+// let rules = getRules.innerText;
+//
+// // -- замініть текст параграфа з id 'content' на будь-який інший
+//
+// content.innerText = 'qwerty';
+//
+// // -- замініть текст параграфа з id 'rules' на будь-який інший
+//
+// rules.innerText = 'qwerty';
+//
+// -- змініть кожному елементу колір фону на червоний
+//
+// content.style.backgroundColor = 'red';
+//
+// // -- змініть кожному елементу колір тексту на синій
+//
+// rules.style.backgroundColor = 'blue';
+//
+// // -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+//
+// const el = document.getElementById('rules');
+// console.log(el.classList);
+//
+// // -- поміняти колір тексту у всіх елементів fc_rules на червоний
+// //
+// let elems = document.getElementsByClassName('fc_rules');
+// elems.style.backgroundColor = 'red';
+
