9f46859456182b8b7e1dac560dd6660a18ff7ba6
DianaHolubieva <golubevadiana356@gmail.com>
2022-06-06 20:16:24 +0200

    $

--- miniProject03.06/assets/JS/user-details.js
+++ miniProject03.06/assets/JS/user-details.js
@@ -2,36 +2,35 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => {
     return response.json();
     })
-    .then(user =>{
+    .then(user => {
         const userDetails = document.createElement('div');
         userDetails.classList.add('usersDetails');
         for (const element of user) {
-                const divUserDetails = document.createElement('div');
-                divUserDetails.classList.add('divUserDetails');
-                divUserDetails.innerHTML = `${element.id}, ${element.name}, ${element.username}, ${element.email}, ${element.address.street}, ${element.address.suite}, ${element.address.city}, ${element.address.zipcode}`;
-                const buttPost = document.createElement('button');
-                buttPost.addEventListener('click', showPosts);
-                buttPost.innerHTML = "post of current user";
-                userDetails.appendChild(divUserDetails);
-                userDetails.appendChild(buttPost);
-                document.body.appendChild(userDetails);
+            const divUserDetails = document.createElement('div');
+            divUserDetails.classList.add('divUserDetails');
+            divUserDetails.innerHTML = `${element.id}, ${element.name}, ${element.username}, ${element.email}, ${element.address.street}, ${element.address.suite}, ${element.address.city}, ${element.address.zipcode}`;
+            const buttPost = document.createElement('button');
+            // buttPost.addEventListener('click', () => {
+            //     fetch('https://jsonplaceholder.typicode.com/users/${user.id}/posts')
+            //         .then(response => {
+            //             return response.json();
+            //         })
+            //         .then(posts => {
+            //             const divPosts = document.createElement('div');
+            //             divPosts.classList.add('divPosts');
+            //             for (const post of posts) {
+            //                 const divPost = document.createElement('div');
+            //                 divPost.classList.add('divUserDetails');
+            //                 divPost.innerHTML = `<div>${post.title}</div>`;
+            //                 divPosts.appendChild(divPost);
+            //                 document.body.appendChild(divPosts);
+            //
+            //             }
+            buttPost.innerHTML = `<a href="post-details.html">post of current user</a>`
+            userDetails.appendChild(divUserDetails);
+            userDetails.appendChild(buttPost);
+            document.body.appendChild(userDetails);
         }
-    })
+    });
 
-function showPosts(){
-    fetch('https://jsonplaceholder.typicode.com/users/USER_ID/posts')
-        .then(response => {
-            return response.json();
-        })
-        .then(posts =>{
-            const divPosts = document.createElement('div');
-            divPosts.classList.add('divPosts');
-            for (const post of posts) {
-                const divPost = document.createElement('div');
-                divPost.classList.add('divUserDetails');
-                divPost.innerHTML = ``;
-                divPosts.appendChild(divPost);
-                document.body.appendChild(divPosts);
-            }
-        })
-}
+
