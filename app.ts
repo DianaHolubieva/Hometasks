ca6a552ebc5de850e42498197cb742f921a1456b
DianaHolubieva <golubevadiana356@gmail.com>
2022-07-26 15:25:47 +0200

    $

--- app.ts
+++ app.ts
@@ -1,51 +1,59 @@
 interface IMission {
     mission_name: string,
     launch_date_local: number,
-    launch_site: {
-        site_name_long: string
-    },
-    links: {
-        article_link: null,
-        video_link: string
-    },
-    rocket: {
-        rocket_name: string,
-        first_stage: {
-            cores: [
-                {
-                    flight: number,
-                    core: {
-                        reuse_count: number,
-                        status: string
-                    }
-                }
-            ]
-        },
-        second_stage: {
-            payloads: [
-                {
-                    payload_type: string,
-                    payload_mass_kg: number,
-                    payload_mass_lbs: number
-                }
-            ]
-        }
-    }
+    launch_site: ILaunchSite,
+    links: ILinks,
+    rocket: IRocket
+
 }
 
-const user: object = {
-    name: string,
-    age:number,
-    gender:string
+interface ILaunchSite {
+    site_name_long: string;
 }
-function sum(a: number, b: number){
-    return Number(a+b)
+
+interface ILinks{
+    article_link: null,
+    video_link: string
 }
-function showSum(a: number, b: number){
+
+interface IRocket  {
+    rocket_name: string;
+    first_stage: ICores;
+    second_stage: IPlayloads;
+
+}
+
+interface ICores {
+    flight: number;
+    core: ICore;
+}
+
+interface ICore {
+    reuse_count: number;
+    status: string;
+}
+
+interface IPlayloads {
+    payload_type: string;
+    payload_mass_kg: numer;
+    payload_mass_lbs: numer;
+}
+
+interface IUser {
+    name:string;
+    age:number;
+    gender:string;
+}
+
+function sum(a:number, b:number):number{
+    return a+b;
+}
+
+function showSum(a:number, b:number):void{
     console.log(a + b);
-}
 
-function incAge(someUser: object, inc: number){
-    someUser.age+=inc
-    return someUser
+
+function incAge(someUser:IUser, inc:number):IUser{
+    someUser.age+=inc;
+    return someUser;
 }
